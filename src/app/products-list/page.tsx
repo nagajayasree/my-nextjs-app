ebd844ed200a0b4a5a785944a6bee6c56d8f3814
JayaSree <js@eventdesk.co>
2023-11-02 10:14:30 -0400

    code changes

--- src/app/products-list/page.tsx
+++ src/app/products-list/page.tsx
@@ -1,29 +1,50 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import styles from './page.module.css';
+// 'use client';
+// import React, { useState, useEffect } from 'react';
+// import styles from './page.module.css';
 
-export default function ProductsList() {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://dummyjson.com/products');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const result = await response.json();
-      setData(result);
-    };
-    fetchData().catch((e) => {
-      console.error('An error occurred while fetching the data: ', e);
-    });
-  }, []);
+// export default function ProductsList() {
+//   const [data, setData] = useState();
 
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await fetch('https://dummyjson.com/products');
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       const result = await response.json();
+//       setData(result);
+//     };
+//     fetchData().catch((e) => {
+//       console.error('An error occurred while fetching the data: ', e);
+//     });
+//   }, []);
+
+//   console.log(data);
+
+//   return (
+//     <div>
+//       <h1 className={styles.main}>Products</h1>
+//     </div>
+//   );
+// }
+
+const fetchData = async () => {
+  const response = await fetch('https://dummyjson.com/products');
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const result = await response.json();
+  return result;
+};
+
+export default async function Products() {
+  const data = await fetchData();
   console.log(data);
-
   return (
     <div>
-      <h1 className={styles.main}>Products</h1>
+      <h1>Products</h1>
+      <div>{JSON.stringify(data, null, 2)}</div>
     </div>
   );
 }
